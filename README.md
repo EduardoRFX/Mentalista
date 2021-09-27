1a83b1708d9a4ce6e0f8d7f472ceb8056618ff76
EduardoRFX <skyyyta@gmail.com>
2021-09-27 11:05:33 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mentalista
+- Advinhe um número de 0 a 10 com 3 tentativas.
+
+<img src="img/Mentalista.png">
---
+++ index.js
@@ -0,0 +1,32 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+console.log(numeroSecreto);
+var tentativas = document.getElementById("tentativas");
+var quantidadeTentativas = 3;
+tentativas.innerHTML = "Você tem " + quantidadeTentativas + " tentativas.";
+function Chutar(){
+    var chute = parseInt(document.getElementById("valor").value);
+    var elementoResultado = document.getElementById("resultado");
+    console.log(chute);
+    if (chute == numeroSecreto){
+        elementoResultado.innerHTML = "Você Acertou!!";
+        console.log("acertou");
+    } else if(chute > 10 || chute < 0){
+        elementoResultado.innerHTML = "Digite um numero de 0 a 10!";
+        console.log("Digite um numero de 0 a 10!");
+
+    } else {
+        elementoResultado.innerHTML = "Você Errou!!";
+        console.log("errou!");
+    }
+
+    quantidadeTentativas = quantidadeTentativas-1;
+    tentativas.innerHTML = "Você tem " + quantidadeTentativas + " tentativas.";
+
+    if (quantidadeTentativas ==0){
+        elementoResultado.innerHTML = "Suas tentativas acabaram!!";
+        document.getElementById("botao").disabled = true;
+    }
+
+}
+
+
